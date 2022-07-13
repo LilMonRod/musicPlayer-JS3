b8ec78d81f60122c9a2bace8a2dedc2d45b04a78
l.montoya.rodriguez <l.montoya.rodriguez@accenture.com>
2022-07-13 13:49:00 -0600

    fixing data to console log

--- js/tagManager.js
+++ js/tagManager.js
@@ -3,10 +3,11 @@ dataLayer = [];
 const songList = document.getElementsByClassName('list-item');
 
 console.log(songList);
+
 for (let i = 0; i < songList.length; i++) {
     songList[i].addEventListener("click", function() {
-        console.log(i.children);
-        console.log(i.childNode);
+        console.log(songList[i].children);
+        console.log(songList[i].childNode);
         dataLayer.push({'musicName': userMusicCategory, 'artist': userArtist});
     });
 }
