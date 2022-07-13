f4c53a25ed274f3ce35872be34f5c7ce9d07175b
l.montoya.rodriguez <l.montoya.rodriguez@accenture.com>
2022-07-13 13:33:31 -0600

    formula to save click artist

---
+++ js/tagManager.js
@@ -0,0 +1,12 @@
+dataLayer = [];
+
+const songList = document.getElementsByClassName('list-item');
+
+
+for (let i = 0; i < songList.length; i++) {
+    songList[i].addEventListener("click", function() {
+        console.log(i.children);
+        console.log(i.childNode);
+        dataLayer.push({'musicName': userMusicCategory, 'artist': userArtist});
+    });
+}
