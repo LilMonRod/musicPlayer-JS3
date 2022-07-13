478ea2737f1342d99902bd1387a5b7478872e552
l.montoya.rodriguez <l.montoya.rodriguez@accenture.com>
2022-07-13 15:20:28 -0600

    saving data layer for google tag manager

--- js/tagManager.js
+++ js/tagManager.js
@@ -2,18 +2,22 @@ dataLayer = [];
 
 const songList = document.getElementsByClassName('list-item');
 let containerSongData = "";
+let songData = "";
+let userMusicName = "";
+let userArtist = "";
 
 window.addEventListener('click', function(e) {
     if (e.path[0].dataset.index != undefined ) {
         containerSongData = e.path[0];
-        console.log(e.path[0]);
     } else if (e.path[1].dataset.index != undefined ) {
         containerSongData = e.path[1];
-        console.log(e.path[1]);
     } else if (e.path[2].dataset.index != undefined ) {
         containerSongData = e.path[2];
-        console.log(e.path[2]);
     }
-
-    dataLayer.push({'musicName': userMusicCategory, 'artist': userArtist});
+    
+    songData = containerSongData.children[1].children[0].innerText
+    userArtist = songData.split('-')[1];
+    userMusicName = songData.split('-')[0];
+    
+    dataLayer.push({'musicName': userMusicName, 'artist': userArtist});
 });
